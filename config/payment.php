ad3862c419293407f886a03dd7614b7ef9c83994
julio-DevI <julio.a.valenca@gmail.com>
2023-09-15 10:11:45 -0300

    fixing the word in the payment file in config directory

--- config/payment.php
+++ config/payment.php
@@ -1,6 +1,6 @@
 <?php
     return [
-        "mecardopago" => [
+        "mercadopago" => [
             'public_key' => env('VITE_MERCADO_PAGO_PUBLIC_KEY'),
             'access_token' => env('MERCADO_PAGO_ACCESS_TOKEN'),
             'buyer_email' => env('MERCADO_PAGO_BUYER_EMAIL'),
