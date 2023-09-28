fff895fa78b66aa987f5c01d248819f38dd1ab4f
julio-DevI <julio.a.valenca@gmail.com>
2023-09-28 16:47:56 -0300

    create the payment method enum

--- app/Livewire/Checkout.php
+++ app/Livewire/Checkout.php
@@ -7,6 +7,7 @@ use App\Exceptions\PaymentException;
 use App\Livewire\Forms\AddressForm;
 use App\Livewire\Forms\UserForm;
 use App\Services\CheckoutService;
+use App\Services\OrderService;
 use App\Services\UserService;
 use Livewire\Component;
 
@@ -45,12 +46,12 @@ class Checkout extends Component
     }
 
 
-    public function pixOrBankSlipPayment(CheckoutService $checkoutService, $data, UserService $userService)
+    public function pixOrBankSlipPayment(CheckoutService $checkoutService, $data, UserService $userService, OrderService $orderService)
     {
         try{
             $payment = $checkoutService->pixOrBankSlipPayment($data, $this->user->all(), $this->address->all());
             $user = $userService->store($this->user->all(), $this->address->all());
-
+            $order = $orderService->update($this->cart['id'], $payment, $user, $this->address);
         } catch (PaymentException $e){
             $this->addError('payment', $e->getMessage());
         }catch (\Exception $e){
--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -6,10 +6,36 @@
 
     class OrderService
     {
-        public function update($order_id, $payment, $user): Order
+        public function update($order_id, $payment, $user, $address): Order
         {
-            Order::find($order_id);
+            $order = Order::find($order_id);
+            $order->user_id = $user->id;
+            $order->status = OrderStepsEnum::parse($payment->status);
+            $order->save();
 
-            $order->status = OrderStepsEnum::parse();
+            $order->payment()->create([
+                'external_id' => $payment->id,
+                'method' => PaymentMethodEnum::parse($payment->payment_type_id),
+                'status' => PaymentStatusEnum::parse($payment->status),
+                'installments' => $payment->installments,
+                'approved_at' => $payment->date_approved ?? null,
+                'qr_code_64' => $payment?->point_of_interaction?->transaction_data?->qr_code_base64,
+                'qr_code' => $payment?->point_of_interaction?->transaction_data?->qr_code ?? null,
+                'ticket_url' => $payment?->point_of_interaction?->transaction_data?->ticket_url ?? $payment?->transaction_details?->external_resource_url,
+            ]);
+
+            $order->shipping()->create([
+                'address' => $address['address'],
+                'number' => $address['number'],
+                'complement' => $address['complement'],
+                'district' => $address['district'],
+                'city' => $address['city'],
+                'state' => $address['state'],
+                'zipcode' => $address['zipcode'],
+            ]);
+
+            $order->load(['payments', 'shippings']);
+
+            return $order;
         }
     }
